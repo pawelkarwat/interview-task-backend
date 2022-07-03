9e81f06b73781dd03e0ad67ae94e87fc9edd9d82
pawelkarwat <pawelkarwat9@gmail.com>
2022-07-03 20:24:38 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-import * as multer from 'multer';
-import * as express from 'express';
 import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
-  // app.use(bodyParser.json());
-  // app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use(multer().array());
-  // app.use(express.static('public'));
   await app.listen(3000);
 }
 bootstrap();
